6d605649eea8b0500e690764b4e85dee9208e1cd
mnikn <329121948@qq.com>
2017-12-30 14:19:34 +0800

    [feature]create project

---
+++ main.ts
@@ -0,0 +1,39 @@
+let electron = require('electron');
+let app = electron.app;
+let BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+function createWindow() {
+
+  app.setName('hexo-manager');
+
+  mainWindow = new BrowserWindow({
+    width: 1366,
+    height: 768,
+    minWidth: 1024,
+    minHeight: 800,
+    frame: true,
+    center: true
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
